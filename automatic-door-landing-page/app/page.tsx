37907bf1c149a9acb8bf178129219d4bb72e484d
Muyubu1 <muyubu1@gmail.com>
2026-01-23 20:29:58 +0300

    remove: hangar door reveal intro animation

--- automatic-door-landing-page/app/page.tsx
+++ automatic-door-landing-page/app/page.tsx
@@ -9,22 +9,19 @@ import FAQSection from "@/components/faq-section"
 import Footer from "@/components/footer"
 import WhatsAppButton from "@/components/whatsapp-button"
 import CallButton from "@/components/call-button"
-import { HangarDoorReveal } from "@/components/ui/hangar-door-reveal"
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <HangarDoorReveal>
-        <Header />
-        <HeroSection />
-        <ProductsSection />
-        <FeaturesSection />
-        <AboutSection />
-        <WhyUsSection />
-        <ProjectSolutionsSection />
-        <FAQSection />
-        <Footer />
-      </HangarDoorReveal>
+      <Header />
+      <HeroSection />
+      <ProductsSection />
+      <FeaturesSection />
+      <AboutSection />
+      <WhyUsSection />
+      <ProjectSolutionsSection />
+      <FAQSection />
+      <Footer />
       <WhatsAppButton />
       <CallButton />
     </main>
